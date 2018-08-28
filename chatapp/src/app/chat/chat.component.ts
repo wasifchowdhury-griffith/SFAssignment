4d8268a4f4520388377ebc795999e0d3bd1c28e5
Wasif Chowdhury <Wasif@MacBook-Pro.local>
2018-08-28 18:10:14 +1000

    working copy

--- chatapp/src/app/chat/chat.component.ts
+++ chatapp/src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../socket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  username: string;
+  messages=[];
+  message;
+  connection;
 
-  constructor() { }
+  constructor(private sockServer: SocketService, private router:Router) { }
 
   ngOnInit() {
+    if(!sessionStorage.getItem('username')){
+      console.log('Not valid login');
+      this.router.navigateByUrl('home');
+    } else {
+      this.username = sessionStorage.getItem('username');
+      console.log("Chat session started for user: " + this.username);
+      this.connection = this.sockServer.getMessages().subscribe(message=>{
+        this.messages.push(message);
+        this.message = "";
+      });
+    }
   }
+
+  sendMessage(){
+    this.sockServer.sendMessage('[' + this.username + ']:' + this.message);
+  }
+
+  ngOnDestroy(){
+    if(this.connection){
+      this.connection.unsubscribe();
+    }
+  }
+
+  logout(){
+    sessionStorage.clear();
+    this.router.navigateByUrl('home');
+  }
+
 
 }
---
+++ chatapp/src/app/socket.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private url = 'http://localhost:3000';
+  private socket;
+
+  constructor() { }
+
+  sendMessage(message){
+    console.log('sendMessage()');
+    this.socket.emit('add-message', message);
+  }
+
+  getMessages(){
+    console.log('getMessages()');
+    this.socket = io(this.url);
+
+    let observable = new Observable(observer => {
+      this.socket.on('message', (data) => {
+        console.log("Received message from Websocket Server")
+        observer.next(data);
+      })
+      return()=>{
+        this.socket.disconnect();
+      }
+    });
+
+    return observable;
+  }
+}
