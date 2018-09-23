4dcc35108f1c9db2c73075fc56584e6af1f93ef2
Wasif Chowdhury <Wasif@MacBook-Pro.local>
2018-09-23 17:05:24 +1000

    updated userservice

--- chatapp/src/app/services/user.service.ts
+++ chatapp/src/app/services/user.service.ts
@@ -1,80 +1,30 @@
 import { Injectable } from '@angular/core';
-import { User } from '../classes/user'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type' : 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  public users: User[];
-  private nextId: number;
-  public currentUser;
-  public currentUserId;
+  private api:string = 'http://localhost:3000/api/';
 
-  constructor() { 
-    let users = this.getUsers();
+  constructor(private http:HttpClient) {}
 
-    if (users.length == 0) {
-      this.nextId = 0;
-    } else {
-      let maxId = users[users.length - 1].id;
-      this.nextId = maxId + 1;
-    }
+  login(data){
+    let body = JSON.stringify(data);
+    return this.http.post(this.api + 'login', body, httpOptions);
   }
 
-  //gets current user
-  public getCurrentUser(){
-    console.log("Current user is: " + this.currentUser + " " + this.currentUserId);
-    return this.currentUser;
+  create(data){
+    let body = JSON.stringify(data);
+    return this.http.post(this.api + 'user/create', body, httpOptions);
   }
 
-  //retrieves username
-  public getUsername() {
-    console.log(localStorage.getItem('users'));
-    return JSON.parse(localStorage.getItem('users')).user;
-  }
-
-  //function to create new user
-  public createUser(user, email ): void {
-    let usern = new User(this.nextId, user, email);
-    let users = this.getUsers();
-    users.push(usern);
-    this.currentUserId = this.nextId;
-    console.log("new users id is: " + this.currentUserId);
-    this.setLocalStorageUsers(users);
-    this.nextId++;
-    this.currentUser = user;
-    
-    console.log("Current user is: " + this.currentUser);
-  }
-
-  //get users array
-  public getUsers(): User[] {
-    let localStorageItem = JSON.parse(localStorage.getItem('users'));
-    return localStorageItem == null ? [] : localStorageItem.users;
-  }
-
-  //sets current user
-  public setCurrentUser(user){
-    this.currentUser = user;
-    console.log("current user has been set to: " + this.currentUser);
-  }
-
-  //get current user by id
-  public getUserId(){
-    console.log(this.currentUserId);
-    return this.currentUserId;
-  }
-
-  //remove users, requires id
-  public removeUsers(id: number): void {
-    let users = this.getUsers();
-    users = users.filter((user) => user.id != id);
-    this.setLocalStorageUsers(users);
-    console.log("user has been removed");
-  }
-
-  //sets local storage of users, requires user
-  private setLocalStorageUsers(users: User[]): void {
-    localStorage.setItem('users', JSON.stringify({ users: users}));
+  delete(username){
+    return this.http.delete(this.api + 'user/delete/' + username);
   }
 }
